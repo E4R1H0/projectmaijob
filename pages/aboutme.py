eaa31d1c9d773be8ef15fa57cc36072272d23169
EEEE <119985774+E4R1H0@users.noreply.github.com>
2023-11-19 16:37:04 +0700

    Update aboutme.py

--- pages/aboutme.py
+++ pages/aboutme.py
@@ -1,4 +1,17 @@
+import json
+import time
+import requests
 import streamlit as st
+from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie_spinner
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+
 html_1 = """
 <div style="background-color:#363062;padding:5px;border-radius:3px;border-bottom: 4px solid #ffffff;border-top: 4px solid #ffffff;">
 <center><h4>การวิเคราะห์ความรู้สึกของลูกค้าร้านSamsung Thailand</h4><h5>Sentiment Analysis of customers of Samsung Thailand store</h5></center>
@@ -6,3 +19,8 @@ html_1 = """
 """
 st.markdown(html_1, unsafe_allow_html=True)
 st.markdown("")
+
+#st.header("การวิเคราะห์ความรู้สึก(Sentiment Analysis)")
+#st.subheader("Nonthakan Jarpun 🐻 DATA SCIENCE NPRU")
+#st.write("__________________________________________________")
+
