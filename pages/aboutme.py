a48f282eaec7e6f91a3fbaa9706de9b8fa503c40
EEEE <119985774+E4R1H0@users.noreply.github.com>
2023-11-19 16:35:03 +0700

    Update aboutme.py

--- pages/aboutme.py
+++ pages/aboutme.py
@@ -1,3 +1,4 @@
+import streamlit as st
 html_1 = """
 <div style="background-color:#363062;padding:5px;border-radius:3px;border-bottom: 4px solid #ffffff;border-top: 4px solid #ffffff;">
 <center><h4>การวิเคราะห์ความรู้สึกของลูกค้าร้านSamsung Thailand</h4><h5>Sentiment Analysis of customers of Samsung Thailand store</h5></center>
