abaf87bd30c3e79731480adf2bcc846e19b16152
EEEE <119985774+E4R1H0@users.noreply.github.com>
2023-11-19 16:34:21 +0700

    Create aboutme.py

---
+++ pages/aboutme.py
@@ -0,0 +1,7 @@
+html_1 = """
+<div style="background-color:#363062;padding:5px;border-radius:3px;border-bottom: 4px solid #ffffff;border-top: 4px solid #ffffff;">
+<center><h4>การวิเคราะห์ความรู้สึกของลูกค้าร้านSamsung Thailand</h4><h5>Sentiment Analysis of customers of Samsung Thailand store</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
