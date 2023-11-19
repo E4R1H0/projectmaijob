eb6280b47140ae0216321066e09991bc7709cf05
EEEE <119985774+E4R1H0@users.noreply.github.com>
2023-11-19 13:17:56 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -21,7 +21,7 @@ col1, col2,col3 = st.columns(3)
 with col1:
     st.image('./pic/earth.jpg')
 with col2:
-    st.write("------")
+    st.write("  ")
 with col3:
     st.image('./pic/DS1.jpg')
 st.balloons()
