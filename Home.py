36055bf755300abe4fda6c7c4d5396b8c182f068
EEEE <119985774+E4R1H0@users.noreply.github.com>
2023-11-19 13:53:39 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -13,7 +13,7 @@ def load_lottieurl(url: str):
 
 
 html_1 = """
-<div style="background-color:#363062;padding:5px;border-radius:5px;border-bottom: 5px solid #ffffff;border-top: 5px solid #ffffff;">
+<div style="background-color:#363062;padding:5px;border-radius:3px;border-bottom: 4px solid #ffffff;border-top: 4px solid #ffffff;">
 <center><h4>การวิเคราะห์ความรู้สึกของลูกค้าร้านSamsung Thailand</h4><h5>Sentiment Analysis of customers of Samsung Thailand store</h5></center>
 </div>
 """
