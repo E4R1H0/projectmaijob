e3efdc9f554949bfccc7d00e62c7031246d7b58f
EEEE <119985774+E4R1H0@users.noreply.github.com>
2023-11-19 13:03:20 +0700

    Update sentiment.py

--- pages/sentiment.py
+++ pages/sentiment.py
@@ -1,8 +1,1 @@
-col1, col2 = st.columns(2)
-
-with col1:
-    st.write("This is column 1")
-
-with col2:
-    st.background_image('./pic/earth.jpg')
-    st.write("This is column 2")
+st.container(background_image='./pic/earth.jpg')
